6dab059def752721d661fae82fb3c5576ec58b46
Maxime Girard <maxime.girard.cpro@gmail.com>
2023-05-19 15:59:12 +0200

    Initial Commit

---
+++ Reciplease/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Reciplease
+//
+//  Created by Maxime Girard on 19/05/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
